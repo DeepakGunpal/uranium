d13ca5c6eba3da590328dd871b412303f529d1b4
Deepak <deepakgunpal0@gmail.com>
2022-04-11 12:29:30 +0530

    Day16 changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://DeepakGunpal:hdg5NWwcvf2wUDTN@deepakcluster0.hynna.mongodb.net/DeepakGunpalFirstDatabase?retryWrites=true&w=majority",{
+    useNewurlParser: true
+})
+.then( ()=> console.log("MongoDb is connected"))
+.catch( err => console.log(err))
 
 app.use('/', route);
 
